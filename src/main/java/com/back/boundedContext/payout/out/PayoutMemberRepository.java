de10a8cd624034b4aec2d1e78c59d5569eaa0e94
Sunmin <sunmin323233@gmail.com>
2025-12-30 14:52:05 +0900

    0044: 주문 품목 불러온 후 PayoutCandidateItem 생성

--- src/main/java/com/back/boundedContext/payout/out/PayoutMemberRepository.java
+++ src/main/java/com/back/boundedContext/payout/out/PayoutMemberRepository.java
@@ -3,6 +3,8 @@ package com.back.boundedContext.payout.out;
 import com.back.boundedContext.payout.domain.PayoutMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PayoutMemberRepository extends JpaRepository<PayoutMember, Integer> {
+import java.util.Optional;
 
+public interface PayoutMemberRepository extends JpaRepository<PayoutMember, Integer> {
+    Optional<PayoutMember> findByUsername(String holding);
 }
